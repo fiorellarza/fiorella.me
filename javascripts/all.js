d46ad1ac0d34afe3998184afd90962c0c6c0eff9
Damir Kotoric <damir.kotoric@envato.com>
2016-04-03 22:15:40 +1000

    Automated commit at 2016-04-03 12:15:40 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,2 +1,16 @@
-//= require "_jquery.min"
 //= require_tree .
+
+$(document).ready(function() {
+  $.ajax({
+    type: "POST",
+    url: "https://medium.com/@damirkotoric/latest?format=json",
+    data: { "patientID" : 1 },
+    dataType: "json",
+    success: function(jsonData) {
+      alert(jsonData)
+    },
+    error: function() {
+      //alert("Error loading")
+    }
+  })
+})
